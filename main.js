a511bc00738a73ed36c15ecc39c7dff5e711491b
NW <nani.wardhani@outlook.com>
2017-04-12 04:10:14 -0600

    add function getLocation to search for real location

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 $(document).ready(function($) {
 
-        $('a').on('click', function(e) {
-            console.log('I am clicked');
-            getCity();
-        })
+    $('a').on('click', function(e) {
+        getLocation();
+    })
 
-        function getCity() {
-            $.ajax({
-                url: 'https://maps.googleapis.com/maps/api/geocode/json',
-
-                data: {
-                  'latlng': '46.872146, -113.9939982'
-                },
-
-                dataType: 'json',
-
-                success: function(r) {
-                  $('.content').text(r.results[1].formatted_address);
-
-                },
-                error: function(e) {
-                    console.long('error', e);
-                }
-
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(loc) {
+                var latlng = loc.coords.latitude + ',' + loc.coords.longitude;
+                getCity(latlng);
             });
         }
+    }
+
+    function getCity(latlngInput) {
+        $.ajax({
+            url: 'https://maps.googleapis.com/maps/api/geocode/json',
+
+            data: {
+                'latlng': latlngInput
+            },
+
+            dataType: 'json',
+
+            success: function(r) {
+                $('.content').text(r.results[1].formatted_address);
+            },
+            error: function(e) {
+                console.long('error', e);
+            }
+        });
+    }
 });
